81344b2bb5ba3e72e7aadd61b3e2614ae5b31e4b
Angel <angelvazquez470@gmail.com>
2023-03-09 11:47:30 -0600

    base de datos hecha

---
+++ database/migrations/2023_03_09_171125_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('presentation');
+            $table->float('max_quantity');
+            $table->float('min_quantity');
+            $table->float('supply_point');
+            $table->float('cost');
+            $table->foreignId('product_type')
+                    ->constrained()
+                    ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
